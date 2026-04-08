ca2a6ea7a4af7a845f5fb72e0966880a93d12981
zun1que <timur.isanov@gmail.com>
2026-04-08 07:53:22 +0500

    add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['react-icons'],
+  },
+};
+
+export default nextConfig;
